2f750ab64bd9e72249647ea080a5cf7cf7108205
Michael <michaelwongycn@gmail.com>
2021-05-25 12:19:59 +0700

    Release V 1.0

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -12,22 +12,28 @@ export const Project = () => {
 		<Section id="Contact">
 			<Header>Contact Me</Header>
 			<ContactContainer>
-				<ContactItem href="#">
+				<ContactItem
+					href="https://www.linkedin.com/in/michaelwongycn"
+					target="_blank"
+				>
 					<Icon className={"fab fa-linkedin"}></Icon>
 				</ContactItem>
-				<ContactItem>
+				<ContactItem href="https://wa.me/6281803888842" target="_blank">
 					<Icon className={"fas fa-mobile-alt"}></Icon>
 				</ContactItem>
-				<ContactItem>
+				<ContactItem href="mailto:michael073@binus.ac.id" target="_blank">
 					<Icon className={"fas fa-envelope"}></Icon>
 				</ContactItem>
-				<ContactItem>
+				<ContactItem href="https://github.com/michaelwongycn" target="_blank">
 					<Icon className={"fab fa-github"}></Icon>
 				</ContactItem>
-				<ContactItem>
+				<ContactItem href="https://michaelwongycn.github.io/" target="_blank">
 					<Icon className={"fas fa-globe"}></Icon>
 				</ContactItem>
-				<ContactItem>
+				<ContactItem
+					href="https://www.hackerrank.com/michaelwongycn"
+					target="_blank"
+				>
 					<Icon className={"fab fa-hackerrank"}></Icon>
 				</ContactItem>
 			</ContactContainer>
